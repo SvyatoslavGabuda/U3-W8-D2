e2c5df70cc1703320472f25ac30c334ff9291575
SvyatoslavGabuda <sgabuda@gmail.com>
2023-02-07 16:03:53 +0100

    a

---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,7 @@
+const MyFooter = () => (
+  <footer className="d-flex justify-content-center py-4">
+    <p className="m-0">Epicode {new Date().toLocaleDateString()}</p>
+  </footer>
+);
+
+export default MyFooter;
